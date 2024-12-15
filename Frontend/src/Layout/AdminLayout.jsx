44bb3e11989914b11c37a3c028167ee7dee20d95
Sachin-K13Y <sachink13y@gmail.com>
2024-12-15 22:23:07 +0530

    Resolve the error

--- Frontend/src/Layout/AdminLayout.jsx
+++ Frontend/src/Layout/AdminLayout.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Outlet, Link, Navigate } from 'react-router-dom';
 import axiosInstance from '../services';
 
-<<<<<<< HEAD
-
 
 const AdminLayout =  () => {
     const [isAdmin, setIsAdmin] = useState(null); 
@@ -69,57 +67,6 @@ const AdminLayout =  () => {
         </div>
     ):(<Navigate to={"/"}/>);
 
-=======
-const AdminLayout = () => {
- 
-  return (
-    <div className="mt-[100px] min-h-screen bg-[#222831]">
-      <h1 className="text-[#F96D00] text-center py-4 text-[50px] font-semibold">Admin Dashboard</h1>
-      <nav>
-        <ul className="flex justify-center gap-5">
-          <li className="text-center pb-4">
-            <Link to="/admin/addmember">
-              <button className="border-4 text-white bg-[#F96D00] text-[30px] p-4 rounded-3xl">Add Member</button>
-            </Link>
-          </li>
-          <li className="text-center pb-4">
-            <Link to="/admin/updatemember">
-              <button className="border-4 text-white bg-[#F96D00] text-[30px] p-4 rounded-3xl">Update Member</button>
-            </Link>
-          </li>
-          <li className="text-center pb-4">
-            <Link to="/admin/deletemember">
-              <button className="border-4 text-white bg-[#F96D00] text-[30px] p-4 rounded-3xl">Delete Member</button>
-            </Link>
-          </li>
-          <li className="text-center pb-4">
-            <Link to="/admin/addevent">
-              <button className="border-4 text-white bg-[#F96D00] text-[30px] p-4 rounded-3xl">Add Event</button>
-            </Link>
-          </li>
-          <li className="text-center pb-4">
-            <Link to="/admin/updateevent">
-              <button className="border-4 text-white bg-[#F96D00] text-[30px] p-4 rounded-3xl">Update Event</button>
-            </Link>
-          </li>
-          <li className="text-center pb-4">
-            <Link to="/admin/deleteevent">
-              <button className="border-4 text-white bg-[#F96D00] text-[30px] p-4 rounded-3xl">Delete Event</button>
-            </Link>
-          </li>
-          <li className="text-center pb-4">
-            <Link to="/admin/addphoto">
-              <button className="border-4 text-white bg-[#F96D00] text-[30px] p-4 rounded-3xl">Add Photos</button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <Outlet />
-      </div>
-    </div>
-  );
->>>>>>> 8650b216410f20af75144cd548ab09d3b87fd3a4
-};
+  }
 
-export default AdminLayout;
+export default AdminLayout
