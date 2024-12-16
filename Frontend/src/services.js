8e85d957b274d37ac7239d67493b8f225159394f
Sachin-K13Y <163808022+Sachin-K13Y@users.noreply.github.com>
2024-12-16 08:22:21 +0530

    Update services.js

--- Frontend/src/services.js
+++ Frontend/src/services.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 
-  baseURL: 'https://think-india-backend.onrender.com', 
+  baseURL: 'https://think-india-2-0.onrender.com/', 
 
   headers: {
     'Content-Type': 'application/json',
