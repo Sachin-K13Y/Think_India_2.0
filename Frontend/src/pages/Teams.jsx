f2977d9748a914084baa1de0ef0c42b8d039f9e1
Sachin-K13Y <sachink13y@gmail.com>
2024-12-19 16:38:33 +0530

    done with team card

--- Frontend/src/pages/Teams.jsx
+++ Frontend/src/pages/Teams.jsx
@@ -25,10 +25,10 @@ const Teams = () => {
   }, []);
 
   return (
-    <div className=' bg-gray-900 min-h-screen  px-2 py-4 flex flex-wrap justify-center'>
+    <div className=' bg-gray-900 min-h-screen mt-[100px]  px-3 py-10 flex flex-wrap justify-center'>
       {teamSections.map(({ title, filter }) => (
         <div key={title}>
-          <h1 className='text-[#F2F2F2] text-3xl font-semibold border-l-4 border-[#F96D00] px-3'>{title}</h1>
+          <h1 className=' text-[#F2F2F2] text-3xl font-semibold border-l-4 border-[#F96D00] px-3 '>{title}</h1>
           <div className='bg-gray-900 px-20 py-4 flex flex-wrap justify-center'>
             {teamData
               .filter((member) => !filter || member.team === filter)
